91eecf2cffd016ff1ecd047d568abd9d82b0c72f
Bohdan Benkevych <bohdan.benkevych@inveritasoft.com>
2022-02-23 13:51:00 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import classNames from "classnames";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [side, _setSide] = useState(null);
+  const [result, setResult] = useState();
+  const playRef = useRef(null);
+  const sideRef = useRef(null);
+  const timeoutRef = useRef(null);
+
+  const setSide = (s) => {
+    sideRef.current = s;
+    _setSide(s);
+  };
+
+  const show = () => {
+    setSide(Math.round(Math.random()));
+    timeoutRef.current = setTimeout(() => {
+      notify("fail");
+    }, 1000);
+  };
+
+  const notify = (res) => {
+    setSide(null);
+    setResult(res);
+    playRef.current = null;
+    setTimeout(() => {
+      setResult("");
+      play();
+    }, 300);
+  };
+
+  const checkResult = (e) => {
+    clearTimeout(timeoutRef.current);
+    clearTimeout(playRef.current);
+    switch (e.keyCode) {
+      case 70:
+        notify(sideRef.current === 0 ? "success" : "fail");
+        break;
+      case 74:
+        notify(sideRef.current === 1 ? "success" : "fail");
+        break;
+      default:
+        notify("fail");
+    }
+  };
+
+  const play = () =>
+    !playRef.current &&
+    (playRef.current = setTimeout(show, Math.random() * (3000 - 500) + 500));
+
+  useEffect(() => {
+    document.addEventListener("keydown", checkResult);
+    play();
+    return () => {
+      document.removeEventListener("keydown");
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classNames("game", result)}>
+      <div className="container">
+        {side === 0 && <div className="box">F</div>}
+      </div>
+      <div className="container">
+        {side === 1 && <div className="box">J</div>}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
